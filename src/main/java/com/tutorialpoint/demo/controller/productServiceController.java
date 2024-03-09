ffcdb9314d646cffc223afa0a0aee1524e0d6960
Saddam-Pc <saddam8101.sk@gmail.com>
2024-03-09 15:46:20 +0530

    added deployment and service yaml file

--- src/main/java/com/tutorialpoint/demo/controller/productServiceController.java
+++ src/main/java/com/tutorialpoint/demo/controller/productServiceController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tutorialpoint.demo.exception.ProductNotFoundException;
 import com.tutorialpoint.demo.model.Product;
 import com.tutorialpoint.demo.service.ProductService;
 
